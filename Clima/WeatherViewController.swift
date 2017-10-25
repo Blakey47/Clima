62b8b5e19a20766565ce37a70cfe7ef329e6f892
Darragh Blake <darragh.blake48@gmail.com>
2017-10-25 22:17:56 +0100

    Created the WeatherDataModel

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -24,6 +24,7 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
 
     //TODO: Declare instance variables here
     let locationManager = CLLocationManager()
+    let weatherDataModel = WeatherDataModel()
 
     
     //Pre-linked IBOutlets
@@ -81,8 +82,19 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate {
     //Write the updateWeatherData method here:
     func updateWeatherData(json: JSON) {
         
-        let tempResult = json["main"]["temp"]
+        if let tempResult = json["main"]["temp"].double {
         
+        weatherDataModel.temperature = Int(tempResult - 273.15)
+        
+        weatherDataModel.city = json["name"].stringValue
+        
+        weatherDataModel.condition = json["weather"][0]["id"].intValue
+        
+        weatherDataModel.weatherIconName = weatherDataModel.updateWeatherIcon(condition: weatherDataModel.condition)
+            
+        } else {
+            cityLabel.text = "Weather Unavailable"
+        }
     }
 
     
